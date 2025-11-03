f964e7489fc9cd4fa5afe788ddf7581d3ea1f3a5
Harlan Bullock <harlanbull7@gmail.com>
2025-11-03 15:00:32 -0800

    Setup badge.js and renames achievements.html to badge.html

---
+++ src/badge.js
@@ -0,0 +1,7 @@
+import { onAuthReady } from "./authentication.js";
+import { db } from "./firebaseConfig.js";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+
+function processBadges() {
+    
+}

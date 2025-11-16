d930496b62b4ed05759179f45718f450cf6146e3
jasminefoong <jasfoong19@gmail.com>
2025-11-16 10:18:18 -0800

    redirect user from recycle page to login/signup page if not logged in

--- src/recycleInfo.js
+++ src/recycleInfo.js
@@ -1,12 +1,26 @@
-let dropdowns = document.getElementsByClassName("dropdown");
+import { onAuthReady } from "./authentication.js"
 
-for (let dropdown of dropdowns) {
-  dropdown.addEventListener("click", () => {
-    let arrowIcon = dropdown.querySelector("i");
-    arrowIcon.classList.toggle("fa-angle-down");
-    arrowIcon.classList.toggle("fa-angle-up");
+function initAuthUI() {
+  onAuthReady(async (user) => {
+    if (!user) {
+      location.href = "loginSignup.html";
+      return;
+    } else {
+      let dropdowns = document.getElementsByClassName("dropdown");
 
-    let dropdownContents = dropdown.nextSibling.nextSibling;
-    dropdownContents.classList.toggle("hidden!");
+      for (let dropdown of dropdowns) {
+        dropdown.addEventListener("click", () => {
+          let arrowIcon = dropdown.querySelector("i");
+          arrowIcon.classList.toggle("fa-angle-down");
+          arrowIcon.classList.toggle("fa-angle-up");
+
+          let dropdownContents = dropdown.nextSibling.nextSibling;
+          dropdownContents.classList.toggle("hidden!");
+        });
+      }
+    }
   });
 }
+
+
+document.addEventListener('DOMContentLoaded', initAuthUI);

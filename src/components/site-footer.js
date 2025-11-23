472fbb25a91ebdeab597dbe52a5f5c64cb6d2b5f
jasminefoong <jasfoong19@gmail.com>
2025-11-23 13:36:21 -0800

    increase padding for footer

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,7 +2,7 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <!-- Footer: single source of truth -->
-            <footer class="mx-16 md:pt-20 pt-25 pb-5 text-[#6a994e] text-center">
+            <footer class="mx-16 md:pt-20 pt-30 pb-5 text-[#6a994e] text-center">
                 <p class="text-muted">&copy; 2025 BCIT COMP1800 DTC01</p>
             </footer>
         `;
